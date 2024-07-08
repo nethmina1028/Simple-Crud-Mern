cb832a99b3c9ef4eab1c158381c4992990f08bbd
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-08 21:22:43 +0530

    final

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose');
 const Product = require('./models/product.model');
 const app = express()
-
+   
+    
+   //middleware
 app.use(express.json());
+app.use (express.urlencoded({extended: false}));
+
+   
+  //routes
+
+  app.use("/api/products",productRoutes);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -44,6 +53,57 @@ app.post('/api/products', async (req, res) => {
     res.status(500).json({error: error.message});
    }
 });
+
+
+
+
+//update product
+   
+  app.put('/api/product/:id', async (req, res) => {
+   
+    try{
+      const {id} =req.params;
+      const product = await Product.findByIdAndUpdate(id, req.body);
+
+      if(!product){
+        return res.status(404).json({message: 'Product not found'});
+      }
+      
+      const updatedProduct = await Product.findById(id);
+      res.status(200).json(updatedProduct);
+
+    }catch(error){
+     res.status(500).json({error: error.message});
+    }
+    
+    
+  });
+
+
+  //delete product
+   
+  app.delete('/api/product/:id', async (req, res) => {
+   
+    try{
+      const {id} =req.params;
+      const product =await Product.findByIdAndUpdate(id);
+
+      if(!product){
+        return res.status(404).json({message: 'Product not found'});
+      }
+      
+      
+      res.status(200).json({message: 'Product deleted successfully'});
+
+    }catch(error){
+     res.status(500).json({message: error.message});
+    }
+    
+    
+  });
+
+
+
 
 mongoose.connect("mongodb+srv://nethmina1028:Q5VEv6bpArzR7UEW@backenddb.6dfxsrk.mongodb.net/NodeAPI?retryWrites=true&w=majority&appName=BackendDB")
 .then(()=>{
